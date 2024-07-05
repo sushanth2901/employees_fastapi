04c164b41c4530a95f1b56f1ec1f5e5094f0ecea
sushanth2901 <75194546+sushanth2901@users.noreply.github.com>
2024-07-05 17:20:59 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,21 @@
 #bin/bash
 
-touch /tmp/sample_test_file_jenkins
+CPU_THRESHOLD=2
+MEMORY_THRESHOLD=4
 
+EMAIL_RECIPIENTS="sushanth.tudi@modak.com"
+EMAIL_SUBJECT="Resource Utilization Alert"
+
+CPU_UTILIZATION=$(vmstat 1 2 | tail -1 | awk '{print 100 - $15}')
+
+MEMORY_UTILIZATION=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+
+echo "Current CPU Utilization: ${CPU_UTILIZATION}%"
+echo "Current Memory Utilization: ${MEMORY_UTILIZATION}%"
+
+if (( $(echo "$CPU_UTILIZATION > $CPU_THRESHOLD" | bc -l) )) || (( $(echo "$MEMORY_UTILIZATION > $MEMORY_THRESHOLD" | bc -l) )); then
+    echo "Threshold exceeded, sending email notification."
+    echo -e "Resource Utilization Alert:\n\nCPU Utilization: ${CPU_UTILIZATION}%\nMemory Utilization: ${MEMORY_UTILIZATION}%\n\nOne or both of these metrics have exceeded their respective thresholds." | mail -s "$EMAIL_SUBJECT" "$EMAIL_RECIPIENTS"
+else
+    echo "Resource utilization is within acceptable limits."
+fi
